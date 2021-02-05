751b85194527a5f4422815a8336d38620706c982
Abdullah <73560425+zer-far@users.noreply.github.com>
2021-02-05 22:56:48 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,13 +16,19 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
   
-  <form action="https://send.pageclip.co/CP0PWPlRaxnTvzjfPtNxKXAvRhpAasYC/contact" class="pageclip-form" method="post">
-  <input type="email" name="email" value="roscoe@example.com" />
-  <input type="text" name="subject" value="A contact subject" />
-  <button type="submit" class="pageclip-form__submit">
-    <span>Send</span>
-  </button>
+ <link rel="stylesheet" href="style.css">
+<form class="cf">
+  <div class="half left cf">
+    <input type="text" id="input-name" placeholder="Name">
+    <input type="email" id="input-email" placeholder="Email address">
+    <input type="text" id="input-subject" placeholder="Subject">
+  </div>
+  <div class="half right cf">
+    <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+  </div>  
+  <input type="submit" value="Submit" id="input-submit">
 </form>
+
   
   </Layout>
 )
