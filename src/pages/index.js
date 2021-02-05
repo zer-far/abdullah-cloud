5c8208f8bfaeec5c11470b8ace622853f1077ffb
Abdullah <73560425+zer-far@users.noreply.github.com>
2021-02-05 16:38:34 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,14 +17,8 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link> <br />
   
   <form action="https://send.pageclip.co/CP0PWPlRaxnTvzjfPtNxKXAvRhpAasYC/contact" class="pageclip-form" method="post">
-  <!-- Replace these inputs with your own. Make sure they have a "name" attribute! -->
-
-  <!-- It looks like you are creating a contact form. These email and subject fields are special -->
-  <!-- See https://pageclip.co/docs#special-fields for more info -->
   <input type="email" name="email" value="roscoe@example.com" />
   <input type="text" name="subject" value="A contact subject" />
-
-  <!-- This button will have a loading spinner. Keep the inner span for best results. -->
   <button type="submit" class="pageclip-form__submit">
     <span>Send</span>
   </button>
