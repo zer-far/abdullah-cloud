5b033e896887d4d446fc5f8c60a94790cf7d0523
Abdullah <73560425+zer-far@users.noreply.github.com>
2021-02-05 23:16:39 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,20 +16,6 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
   
- <link rel="stylesheet" href="style.css">
-<form class="cf">
-  <div class="half left cf">
-    <input type="text" id="input-name" placeholder="Name">
-    <input type="email" id="input-email" placeholder="Email address">
-    <input type="text" id="input-subject" placeholder="Subject">
-  </div>
-  <div class="half right cf">
-    <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
-  </div>  
-  <input type="submit" value="Submit" id="input-submit">
-</form>
-
-  
   </Layout>
 )
 
