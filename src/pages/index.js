d439cb6ca96316c8a4f2191bcc2765d30692da1a
Abdullah <73560425+zer-far@users.noreply.github.com>
2021-02-06 12:10:56 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,68 +16,6 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
   </Layout>
-  
-  <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Contact</title>
-  
-  <script type="text/javascript">
-function conf()
-{
-var con=confirm("Submitted!");
-}
-</script>
-  
-</head>
-
-<body>
-
-<link href="contact.css" rel="stylesheet">
-
-<div class="fcf-body">
-
-    <div id="fcf-form">
-    <h3 class="fcf-h3">Contact me</h3>
-
-    <form id="fcf-form-id" class="fcf-form-class" method="post" action="https://send.pageclip.co/CP0PWPlRaxnTvzjfPtNxKXAvRhpAasYC"
-        
-        <div class="fcf-form-group">
-            <label for="Name" class="fcf-label">Your name</label>
-            <div class="fcf-input-group">
-                <input type="text" id="Name" name="Name" class="fcf-form-control" required>
-            </div>
-        </div>
-
-        <div class="fcf-form-group">
-            <label for="Email" class="fcf-label">Your email address</label>
-            <div class="fcf-input-group">
-                <input type="email" id="Email" name="Email" class="fcf-form-control" required>
-            </div>
-        </div>
-
-        <div class="fcf-form-group">
-            <label for="Message" class="fcf-label">Your message</label>
-            <div class="fcf-input-group">
-                <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
-            </div>
-        </div>
-
-        <div class="fcf-form-group">
-            <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block" onClick="conf()">
-              <span>Submit</span>
-            </button>
-        </div>
-</form>
-    </form>
-    </div>
-
-</div>
-
-</body>
-</html>
-
 )
 
 export default IndexPage
