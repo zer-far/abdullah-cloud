034bc042c60f2a7bbaeaf5f382325468ba270981
Abdullah <73560425+zer-far@users.noreply.github.com>
2021-05-01 21:27:39 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const IndexPage = () => (
     <p>Welcome abdullah.cloud</p>
     <p>Go to page 2 to see services on this website.</p>
 <a href="https://ostr.io/protected/abdullah.cloud" target="_blank" rel="noopener noreferrer"><img src="https://ostr.io/images/oblt.png" height="32" /></a>
+  <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "1a7debb524eb4cf19151f39a981b0612"}'></script><!-- End Cloudflare Web Analytics -->
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
